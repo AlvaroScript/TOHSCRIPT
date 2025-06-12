80d15e6faa0157c5273e41947590cef080492d50
AlvaroScript <rampe5929@gmail.com>
2025-06-12 15:07:20 +0700

    Update Loaders.lua

--- Main Script/TOH Loader/Loaders.lua
+++ Main Script/TOH Loader/Loaders.lua
@@ -14,7 +14,7 @@ local currentPlaceId = game.PlaceId
 if allowedPlaceIds[currentPlaceId] then
     print("✅ Game ID verified. Loading your script...")
 
-    loadstring(game:HttpGet("https://your-host.com/your-script.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AlvaroScript/TOHSCRIPT/refs/heads/main/Main%20Script/Key/Tower%20Of%20Hell%20Key.lua"))()
 else
     game.Players.LocalPlayer:Kick("wrong game❌")
 end
