a601bb38dd9363ed583b6c29e12b5f153d846c22
AlvaroScript <rampe5929@gmail.com>
2025-06-17 11:14:30 +0700

    Update Loaders.lua

--- Main Script/TOH Loader/Loaders.lua
+++ Main Script/TOH Loader/Loaders.lua
@@ -5,8 +5,6 @@ end
 -- Allowed Place IDs
 local allowedPlaceIds = {
     [1962086868] = true, -- Tower of Hell
-    [3582763398] = true, -- Tower of Hell Pro
-    [5253186791] = true  -- Tower of Hell Appeals
 }
 
 local currentPlaceId = game.PlaceId
@@ -16,5 +14,5 @@ if allowedPlaceIds[currentPlaceId] then
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AlvaroScript/TOHSCRIPT/refs/heads/main/Main%20Script/Key/Tower%20Of%20Hell%20Key.lua"))()
 else
-    game.Players.LocalPlayer:Kick("wrong game❌")
+    game.Players.LocalPlayer:Kick("Wrong game❌")
 end
